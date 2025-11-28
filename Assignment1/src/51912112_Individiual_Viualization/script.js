7770930742922e201c20a0da82ca0805feec6788
NicoReit54 <nico.reit54@gmail.com>
2025-11-28 21:20:36 +0100

    Made it work for now!

--- Assignment1/src/51912112_Individiual_Viualization/script.js
+++ Assignment1/src/51912112_Individiual_Viualization/script.js
@@ -6,9 +6,9 @@
 // TODO: Inspect whether we need the following:
 // Set 21-color scheme https://www.r-bloggers.com/2013/02/the-paul-tol-21-color-salute/
 const color = d3.scaleOrdinal([
-  "#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD", "#117777", 
-  "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", "#777711", "#AAAA44", 
-  "#DDDD77", "#774411", "#AA7744", "#DDAA77", "#771122", "#AA4455", "#DD7788"])
+  "#771155", "#114477", "#44AA77", "#DDAA77", "#AA4488", "#CC99BB",  "#4477AA", "#77AADD", "#117777", 
+  "#44AAAA", "#77CCCC", "#117744",  "#88CCAA", "#777711", "#AAAA44", 
+  "#DDDD77", "#774411", "#AA7744", "#771122", "#AA4455", "#DD7788"])
 
 // We have to adapt the data types as all are strings!
 d3.csv("vis_data.csv", d => ({
@@ -115,8 +115,18 @@ const plotTreeMap = function(data) {
 
   // 2. Create the hierarchical layout with d3.hierarchy
   // Ref: https://d3js.org/d3-hierarchy/hierarchy
-  const root = d3.hierarchy(group)
-    .sum(([key, value]) => value.avg_price) // will be the value visible in the console.log of root
+  // According to docu above (copy&paste)
+  // If data is a Map, it is implicitly converted to the entry [undefined, data], and the children accessor instead defaults to:
+  // function children(d) {
+  //  return Array.isArray(d) ? d[1] : null;
+  //}
+  // This allows us to pass the result of group or rollup to hierarchy
+  // So it checks whether the child is another array and expands it in the case 
+
+  const root = d3.hierarchy(group, ([key, value]) =>
+    value instanceof Map ? Array.from(value) : null
+  )
+  .sum(([key, value]) => value.avg_price);
     //.sort((a, b) => d3.descending(a.value, b.value));
 
   console.log(root) // This format I do not entirely get but I also did not during the tutorial
@@ -126,24 +136,30 @@ const plotTreeMap = function(data) {
   d3.treemap()
    .tile(d3.treemapSquarify)
    .size([width, height])
-   .padding(1)
+   .padding(2)
    (root); // calling it here with root >> that when root existds the d3 treemap is executed
   
-
   // 4. Add leave nodes to the SVG element
   const node = svg.selectAll("a") // TODO look up <a> element in HTML, has something to do with hyperlinks
    .data(root.leaves())
    .join("a")
     .attr("transform", d => `translate(${d.x0}, ${d.y0})`);
   
+  console.log(node);
+  
   node.append("rect")
-   .attr("fill", d => color(d["data"]["Section ID"] - 1))
+   .attr("fill", d => color(d.parent.data[0]))
    .attr("fill-opacity", 0.5)
    .attr("width", d => d.x1 - d.x0)
    .attr("height", d => d.y1 - d.y0);
 
+  console.log(node);
+
   // 5. Add the tooltip to each node
-  //node.append("title").text(d => `${d.data.HS4}\n${d3.format("$,")(d.value)}`);
+  node.append("title")
+  .text(d => `${d.data[0]} (${d.parent.data[0]})
+              \nAvg Price: ${d3.format(".2f")(d.data[1].avg_price)}
+              \nAvg Rating: ${d3.format(".2f")(d.data[1].avg_rating)}`);
 
   // 6. Add text to each node
   node.append("clipPath")
@@ -156,6 +172,6 @@ const plotTreeMap = function(data) {
         // adding a clip-path cuts everything that is below in the html structure so the text does not go over the square!!
         // inspect the text to see exactly, but its just above the actual text value so it makes sense!
        .attr("clip-path", (d, i) => `url(#clip-${i})`) 
-       .html(d => d.value >= 500000000 ? `<tspan x=5 y=15 font-weight="bold">${d.data.HS4}</tspan>
-                   <tspan x=5 y=30 fill-opacity=0.7>${d3.format("$.3s")(d.value)}</tspan>` : ``)
+       .html(d =>`<tspan x=5 y=15 font-weight="bold">${d.data[0]}</tspan>
+                   <tspan x=5 y=30 fill-opacity=0.7>${d3.format("$.3s")(d.data[1].avg_price)}</tspan>`)
 }
