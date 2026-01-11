a7095fc4d2152c82dbe3df28b17b2fa14ba846fb
NicoReit54 <nico.reit54@gmail.com>
2026-01-11 09:22:55 +0100

    Fixed cut off text in treemap

--- Assignment2/src/treemap.js
+++ Assignment2/src/treemap.js
@@ -153,6 +153,72 @@ function createTreemap(container, airbnbData, level1, level2, level3, onCellClic
         .attr("x", 3)
         .attr("y", (d, i) => 12 + i * 10)
         .text(d => d);
+
+    // ========================================
+    // UPDATE MODE (update with transition())
+    // It basically takes what is there and then 
+    // transitions into what is added
+    // ========================================
+
+    // set transition variable
+    const t = d3.transition()
+        .duration(500) // 0.5 sec
+        .ease(d3.easeSin); 
+    
+    node.transition(t)
+        .attr("transform", d => `translate(${d.x0}, ${d.y0})`);
+    
+    node.select("rect").transition(t)
+        .attr("fill", d => treemapColor(d.parent.parent.data[0]))
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0);
+    
+    // tooltips
+    node.select("title").transition(t)
+        .text(d => `L1: ${d.parent.parent.data[0]} // L2: ${d.parent.data[0]} // L3: ${d.data[0]}
+        \nNumber of Units: ${d.data[1].count}
+        \nAvg Price: ${d3.format(".2f")(d.data[1].avg_price)}
+        \nAvg Rating: ${d3.format(".2f")(d.data[1].avg_rating)}
+        \nAvg Bedrooms: ${d3.format(".2")(d.data[1].avg_bedrooms)}
+        \nAvg Beds: ${d3.format(".2")(d.data[1].avg_beds)}`);
+
+    // recalculate the clipPath thingy
+    node.select("clipPath")
+        .attr("id", (d, i) => `clip-${i}`);
+
+    node.select("clipPath rect").transition(t)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0);
+
+    // Remove old text to add it afterwards (we do not need to morph like with the tiles, doesnt make sense for text)
+    node.select("text").remove();
+
+
+    // Append fresh text with fade-in transition
+    node.append("text")
+        .attr("clip-path", (d, i) => `url(#clip-${i})`)
+        // for this style attribute I asked CoPilot for input on how to manage this
+        .style("display", d => (d.x1 - d.x0 < 20 || d.y1 - d.y0 < 20) ? "none" : "block")
+        .style("opacity", 0) // start invisible
+        .html(d => `
+        <tspan x=5 y=15 font-weight="bold">${d.data[0]}</tspan>
+        <tspan x=5 y=30 fill-opacity=0.7> Units: ${d.data[1].count}</tspan>
+        <tspan x=5 y=45 fill-opacity=0.7> Beds: ${d3.format(".3")(d.data[1].avg_beds)}</tspan>
+        <tspan x=5 y=60 fill-opacity=0.7> Rating: ${d3.format(".3")(d.data[1].avg_rating)}</tspan>
+        <tspan x=5 y=75 fill-opacity=0.7>${d3.format("$.3s")(d.data[1].avg_price)}</tspan>
+        `)
+        // apply transition to full opacity
+        .transition(t)
+        .style("opacity", 1); 
+
+    // ========================================
+    // EXIT
+    // Delete what is not anymore here
+    // ========================================
+    node.exit().transition(t)
+        .style("opacity", 0)
+        .remove();
+    
 }
 
 export { createTreemap, buildHierarchyTree };
